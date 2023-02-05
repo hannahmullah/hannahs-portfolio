194fa818e1cf866d455da2663e8aae31b82f080b
Hannah Mullah <hannahmullah@hotmail.com>
2023-02-05 22:26:20 +0000

    'ready for deploy'

---
+++ src/components/About.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div
+            name="about"
+            id="about"
+            className="w-full h-screen bg-[#fff] text-white"
+        > {/*c39a98 */} {/*bg-gradient-to-r from-[#c28688] to-[#d99787]*/}
+            <div className="flex flex-col justify-center items-center w-full h-full">
+                <div className=" py-16 rounded-md bg-[#c28688] flex flex-col justify-center items-center w-4/6">
+                    <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-4">
+                        <div className="sm:text-right pb-8 pl-4">
+                            <p className="text-4xl font-bold inline border-b-4 border-[#0bbbc8]">
+                                About
+                            </p>
+                        </div>
+                        <div></div>
+                    </div>
+                    <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+                        <div className="sm:text-right text-4xl font-bold">
+                            <p>
+                                Hi. I'm Hannah Mullah, nice to meet you. Please take a
+                                look around.
+                            </p>
+                        </div>
+                        <div>
+                            <p>
+                                {" "}
+                                Once the decision was made to transition careers from healthcare to software development,
+                                there was no stopping. I knew that I wanted to express my creativity and to start a new challenge
+                                in the world of technology, even though the process wasn't going to be easy. However, the knowledge
+                                that the long term outcome of becoming a junior developer, got rid of any doubts. From starting on YouTube,
+                                to Udemy tutorials, to then completing an intensive Software Development course covering front and back-end
+                                technologies. The extensive journey has allowed me to develop vital skills required in the software
+                                development industry, alongside my already developed skills of management, communication and working
+                                in one of the most busiest industries in the UK.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import me from '../assets/me.jpg';
+
+import { Link } from "react-scroll";
+
+const Home = () => {
+    return (
+        <div
+            name="home"
+            className="h-screen w-full bg-[#fff]"
+        >
+            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+                <div className="flex flex-col justify-center h-full">
+                    <h2 className="text-4xl sm:text-7xl font-bold text-[#c28688]">
+                        Junior developer
+                    </h2>
+                    <p className="text-gray-500 py-4 max-w-md">
+                        An aspiring junior developer currently working on web development.
+                        At present, I love to work on web application using technologies like
+                        React, Tailwind, Node.js and Mongodb.
+                    </p>
+                    <div>
+                        <Link
+                            to="about"
+                            smooth
+                            duration={500}
+                            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-[#0bbbc8] to-[#a9c7ab] cursor-pointer"
+                        >
+                            About Me
+                            <span className="group-hover:rotate-90 duration-300">
+                                <HiArrowNarrowRight size={25} className="ml-3" />
+                            </span>
+                        </Link>
+                    </div>
+                </div>
+                <div>
+                    <img
+                        src={me}
+                        alt="my profile"
+                        className="rounded-full mx-auto w-2/3 md:w-98"
+                    />
+                </div>
+            </div>
+        </div >
+    );
+};
+export default Home;
